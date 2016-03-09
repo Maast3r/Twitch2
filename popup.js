e806decd1377bae5f6ed93e7edeaba81cef9b196
Andrew Ma <maastergotmail@gmail.com>
2016-03-08 20:53:36 -0500

    no need for a css file

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 function addStream(e){
 	var inputValue = document.getElementById("input").value;
-	chrome.tabs.insertCSS(null, {file: "player.css"});
 	chrome.tabs.executeScript(null, {
 		code: "var name = " + JSON.stringify(inputValue)}, function(){
 		chrome.tabs.executeScript(null, {file: "addStream.js"});
