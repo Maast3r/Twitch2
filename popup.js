a678148e5c7ff11630c1c9da50b8b85f78f2baaf
andrew <maastergotmail@gmail.com>
2017-04-14 22:44:25 -0400

    version two, compliant with iframes (twitch's new api)

--- popup.js
+++ popup.js
@@ -1,19 +1,17 @@
 window.addEventListener('load', function(evt) {
-	function addStream(e){
-		var inputValue = document.getElementById("streamToAdd").value;
-		chrome.tabs.insertCSS(null, {file: "material.indigo-pink.min.css"});
-		chrome.tabs.executeScript(null, {
-			code: "var name = " + JSON.stringify(inputValue)}, function(){
-				chrome.tabs.executeScript(null, {file: "material.min.js"});
-				chrome.tabs.executeScript(null, {file: "addStream.js"});
-		});
-	}
+    function addStream(){
+        var inputValue = document.getElementById("stream-to-add").value;
+        chrome.tabs.executeScript(null, {
+            code: "var name = " + JSON.stringify(inputValue)}, function() {
+                chrome.tabs.executeScript(null, {file: "addStream.js"});
+        });
+    }
 
-	document.getElementById('streamToAdd').onkeypress = function(e){
-		if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-		if (keyCode == 13) {
-	    addStream(e);
-	  }
-	};
+    document.getElementById('stream-to-add').onkeypress = function(e){
+        if (!e) e = window.event;
+        var keyCode = e.keyCode || e.which;
+        if (keyCode == 13) {
+            addStream(e);
+      } 
+    };
 });
